1f5553df9c8505a9998b704ffb5194ab10769d6b
Jason Yang <jasonyang187@outlook.com>
2016-10-09 14:39:47 +0800

    调整价格日历

--- common/js/calendar.Q3.js
+++ common/js/calendar.Q3.js
@@ -9,8 +9,11 @@
             weekNames: ['日', '一', '二', '三', '四', '五', '六'],
             $container:'',
             date : '',
-            type : 'normal',
+            type : 'tourism',
+            stockType : 'normal', // hotel 酒店日历
+            postData: {},
             month : '3',
+            isReload : false,
             selectedFun : function() {}
         };
         params = params || {};
@@ -20,10 +23,11 @@
             }
         }
         _self.params = params;
-        _self.index = 1; // 请求月份次数
+        _self.index = _self.params.index || 1; // 请求月份次数
+        _self.nextDate = new Date(_self.params.date.split("-")[0],_self.params.date.split("-")[1]-1,+_self.params.date.split("-")[2] + 1); // 当前日期的下一天
 
         _self.get_title_html = function(title) {
-            return '<div class="calendar-title">' + title + '</div>';
+            return '<div class="calendar" id="'+_self.index+'"><div class="calendar-title">' + title + '</div>';
         };
 
         _self.get_weeks_html = function() {
@@ -35,20 +39,29 @@
             return html;
         };
         _self.getDayData = function(data,day) {
-            for(var i = 0; i < data.length; i++) {
-                if(data[i].day.split("-")[2] == day) {
-                    var price = data[i].price;
-                    var count = data[i].count;
-                    var cardExchangeDays = data[i].cardExchangeDays;
+            if(data && data.length) {
+                for(var i = 0; i < data.length; i++) {
+                    if(data[i].date.split("-")[2] == day) {
+                        var price = data[i].adultSettlement;
+                        var count = data[i].availableNum;
+                        var cardExchangeDays = data[i].cardExchangeDays;
+                    }
+                    var date = data[i].date.split("-")[0] + '-' + data[i].date.split("-")[1] + '-' + day;
                 }
-                var date = data[i].day.split("-")[0] + '-' + data[i].day.split("-")[1] + '-' + day;
+                return {
+                    price : price || 0,
+                    count : count || 0,
+                    date : date,
+                    cardExchangeDays : cardExchangeDays || 0
+                }
+            }  else {
+                return {
+                    price : 0,
+                    count : 0,
+                    date : _self.dateArray[0] + '-' + _self.dateArray[1] + '-' + day
+                }
             }
-            return {
-                price : price || 0,
-                count : count || 0,
-                date : date,
-                cardExchangeDays : cardExchangeDays || 0
-            }
+
         };
         _self.render = function() {
             var html = '';
@@ -61,64 +74,86 @@
             var days = currentDate.getDate();
             var start = _self.dateArray[0] + '-' + _self.dateArray[1] + '-' + '1';
             var end = _self.dateArray[0] + '-' + _self.dateArray[1] + '-' + days;
+            _self.params.postData.start = start;
+            _self.params.postData.end = end;
             $.ajax({
                 type:'POST',
-                url:'/js/data.json',
+                url:_self.params.url,
                 beforeSend : function(){
-
+                    // 进页面提示选择入住日期
+                    if(_self.index == 1) {
+                        $.preloader.show();
+                    }
                 },
-                data:{
-                    id:_self.params.id,
-                    start:start,
-                    end:end,
-                    agreementProductId:_self.params.agreementProductId,
-                    type:_self.params.type
-                },
+                data:_self.params.postData,
                 success: function(data) {
                     var jsonData = JSON.parse(data);
-                    if(jsonData.code == 0) {
-                        if(data.length) {
-                            days_array.forEach(function(day_tr) {
-                                html += '<div class="calendar-row">';
-                                day_tr.forEach(function(day_td) {
+                    if(jsonData.code == 0 || jsonData.code == 2) {
+                        days_array.forEach(function(day_tr) {
+                            html += '<div class="calendar-row">';
+                            day_tr.forEach(function(day_td) {
 
-                                    var item = _self.getDayData(data,day_td);
-                                    var priceStr = item.price? '￥' + item.price : '-';
-                                    priceStr = day_td ? priceStr : '';
+                                var item = _self.getDayData(jsonData.data,day_td);
+                                var priceStr = item.price? '￥' + item.price : '-';
+                                priceStr = day_td ? priceStr : '';
 
-                                    html += '<div class="calendar-col ' +
-                                        (item.count ? 'available' : 'disabled') +
-                                        (_self.params.type == 'normal' && item.count && _self.params.date.split('-')[0] + '-' + _self.params.date.split('-')[1] + '-' + (+_self.params.date.split('-')[2] + 1) == _self.dateArray[0] + '-' + _self.dateArray[1] + '-' + day_td ? 'selected' : '') + '"' +
-                                        'data-stock="'+ item.count +'" ' +
-                                        'data-date="'+item.date+'">' +
-                                        '<div class="date">' + day_td + '</div>' +
-                                        '<div class="price">' + priceStr + '</div>' +
-                                        '</div>'
-                                });
-                                html +=  '</div>';
+                                html += '<div class="calendar-col ' +
+                                    (item.count ? 'available' : 'disabled') +
+                                    (_self.index == 1 && _self.dateArray[2] == day_td ? ' today' : '') +
+                                    (_self.params.stockType == 'normal' && _self.index == 1 && item.count && _self.nextDate.getTime() == new Date(_self.dateArray[0],_self.dateArray[1]-1,day_td).getTime() ? ' selected' : '') + '"' +
+                                    'data-stock="'+ item.count +'" ' +
+                                    'data-date="'+item.date+'">' +
+                                    '<div class="date">' + (_self.index == 1 && _self.dateArray[2] == day_td ? '今天' : day_td) + '</div>' +
+                                    '<div class="price">' + priceStr + '</div>' +
+                                    '</div>'
                             });
+                            html +=  '</div>';
+                        });
+                        html +=  '</div>';
+                        if($("#"+_self.index).length) {
+                            $("#"+_self.index).html(_self.get_title_html(_self.calendarTitle) + _self.get_weeks_html() + html);
+                        } else {
                             _self.params.$container.append(_self.get_title_html(_self.calendarTitle) + _self.get_weeks_html() + html);
-                            _self.dom_control();
                         }
                     }
                 },
                 error: function() {
-                    var dayHtml = '<div class="no_content">加载价格日历失败，<span class="refresh">点击重试！</span></div>';
-                    _self.params.$container.append(_self.get_weeks_html() + dayHtml);
+                    var dayHtml = '<div class="no_content">加载价格日历失败，<span class="refresh" data-index="'+_self.index+'">点击重试！</span></div>';
+                    _self.params.$container.append(_self.get_title_html(_self.calendarTitle) + _self.get_weeks_html() + dayHtml);
+                    var index = _self.index;
+                    var date = _self.params.date;
+                    $(".refresh[data-index='"+_self.index+"']").on('tap',function() {
+                        _self.params.date = date;
+                        _self.params.index = index;
+                        _self.params.isReload = true;
+                        var calendar = new Calendar(params);
+                        calendar.render();
+                    })
                 },
                 complete : function() {
-                    _self.index++;
-                    if(_self.index <= _self.params.month) {
-                        var currentDate = new Date(_self.dateArray[0],_self.dateArray[1]-1,1);
-                        var currentMonth = currentDate.getMonth() + 1;
-                        currentDate.setMonth(currentMonth);
+                    $.preloader.hide();
+                    if(!_self.params.isReload){
+                        _self.index++;
+                        if(_self.index <= _self.params.month) {
+                            var currentDate = new Date(_self.dateArray[0],_self.dateArray[1]-1,1);
+                            var currentMonth = currentDate.getMonth() + 1;
+                            currentDate.setMonth(currentMonth);
 
-                        _self.params.date = currentDate.getFullYear() + '-' + (currentDate.getMonth()+1) + '-' +currentDate.getDate();
-                        _self.render();
+                            _self.params.date = currentDate.getFullYear() + '-' + (currentDate.getMonth()+1) + '-' +currentDate.getDate();
+                            _self.render();
+                        } else {
+                            $.preloader.hide();
+                            if(_self.params.stockType == 'hotel') {
+                                $.alert({
+                                    msg: '请选择入住时间'
+                                });
+                            }
+                            _self.dom_control();
+                        }
                     }
                 }
             });
-            
+
             return html;
         };
         // 转化二维数组
@@ -181,13 +216,15 @@
                 })
             } else if(_self.params.stockType == 'hotel') {
                 var clickIndex = 1; // 记录点击次数
-                var $start,$end;
+                var $start,$end,startCache,endCache;
 
                 $('.calendar-col').tap(function() {
                     var $this = $(this);
                     if(clickIndex == 1 && $this.hasClass('available')) {
                         $start = $(this);
                         $start.addClass('start selected hotelSelected');
+                        startCache = $start.find('.date').text();
+                        $start.find('.date').text('入住');
                         $.alert({
                             msg: '请选择离店时间'
                         });
@@ -200,7 +237,7 @@
                             $start.removeClass('start selected hotelSelected');
                             $start = $end;
                             $start.addClass('start selected hotelSelected');
-                        } else if(toDate(startDate) < toDate(endDate)){
+                        } else if(toDate(startDate) <= toDate(endDate)){
                             var dayArray = [];
                             $('.calendar-col').forEach(function (days) {
                                 var date = $(days).attr('data-date');
@@ -225,6 +262,8 @@
                                 });
                             } else {
                                 $end.addClass('end selected hotelSelected');
+                                endCache = $end.find('.date').text();
+                                $end.find('.date').text('离店');
                                 dayArray.forEach(function(item) {
                                     $(item).addClass('hotelSelected');
                                 });
@@ -235,7 +274,9 @@
                     } else if(clickIndex == 3 && $this.hasClass('available')) {
                         // 第三次点击则置空之前所选，并作为start日期
                         $start.removeClass('start selected hotelSelected');
+                        $start.find('.date').text(startCache);
                         $end.removeClass('end selected');
+                        $end.find('.date').text(endCache);
                         $('.hotelSelected').removeClass('selected hotelSelected');
                         clickIndex = 2;
                         $start = $(this);
